eea6e3bfe1c6db54012d5c980cbfb8b3c10d6b4e
shobhitsundriyal <shobhitsundriyaldev@gmail.com>
2022-06-01 18:36:51 +0530

    reDone

--- frontend_assignment/pages/form.tsx
+++ frontend_assignment/pages/form.tsx
@@ -1,21 +1,159 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form'
 
 function Form() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = handleSubmit(data => console.log(data));
+	const { register, handleSubmit, formState } = useForm()
+	const onSubmit = handleSubmit((data) => {
+		console.log(data)
+	})
 
-  return (
-    <div className='flex justify-center h-[100vh] items-center'>
+	return (
+		<div className='flex justify-center h-[100vh] items-center'>
+			{/* <form onSubmit={onSubmit}>
+				<input
+					className='inputFeild'
+					{...register('firstName', {
+						required: true,
+						maxLength: 20,
+					})}
+					placeholder={'First Name'}
+				/>
 
-    <form  onSubmit={onSubmit} >
-      <input className="inputFeild" {...register("firstName", { required: true, maxLength: 20 })} placeholder={'First Name'}/>
-      <input className="inputFeild" {...register("lastName", { pattern: /^[A-Za-z]+$/i })} placeholder={'Last Name'}/>
-      <input className="inputFeild" type="number" {...register("age", { min: 18, max: 99 })} placeholder={'Age'}/>
-      <input className="inputFeild" {...register("address")} placeholder={'Address'}/>
-      <input className="inputFeild hover:bg-white hover:text-black transition-colors duration-150" type="submit" />
-    </form>
-    </div>
-  )
+				<input
+					className='inputFeild'
+					{...register('lastName', { pattern: /^[A-Za-z]+$/i })}
+					placeholder={'Last Name'}
+				/>
+				<input
+					className='inputFeild'
+					type='number'
+					{...register('age', { min: 18, max: 99 })}
+					placeholder={'Age'}
+				/>
+				<input
+					className='inputFeild'
+					{...register('address')}
+					placeholder={'Address'}
+				/>
+				<input
+					className='inputFeild hover:bg-white hover:text-black transition-colors duration-150'
+					type='submit'
+				/>
+			</form> */}
+
+			<form
+				onSubmit={onSubmit}
+				className='w-full  flex flex-col items-center space-y-12'
+			>
+				<div className='feild-container relative p-2 w-1/5'>
+					<input
+						type='text'
+						id='fname'
+						required
+						className=' bg-transparent outline-none px-1 peer relative text-slate-800 z-10 font-mono font-semibold'
+						{...register('firstName', {
+							required: true,
+							maxLength: 20,
+							pattern: /^[a-zA-Z]*$/, //only alphabets
+						})}
+					></input>
+					<label
+						// for='name'
+						className='absolute left-4 top-2 text-lime-400 peer-focus:text-sm peer-focus:-top-6 peer-valid:text-sm peer-valid:-top-6 transition-all duration-150'
+					>
+						First Name
+					</label>
+					<div
+						className={`line h-[2px] w-full bg-lime-400 transition-all -z-0 duration-200 ease-out absolute -bottom-0 peer-focus:h-full rounded-sm peer-valid:h-full ${
+							formState.errors.firstName
+								? 'bg-red-400'
+								: 'bg-lime-400'
+						}`}
+					></div>
+				</div>
+
+				<div className='feild-container relative p-2 w-1/5'>
+					<input
+						type='text'
+						id='lname'
+						required
+						className=' bg-transparent outline-none px-1 peer relative text-slate-800 z-10 font-mono font-semibold w-full'
+						{...register('lastName', {
+							required: true,
+							maxLength: 20,
+							pattern: /^[a-zA-Z]*$/,
+						})}
+					></input>
+					<label
+						// for='lname'
+						className='absolute left-4 top-2 text-lime-400 peer-focus:text-sm peer-focus:-top-6 peer-valid:text-sm peer-valid:-top-6 transition-all duration-150'
+					>
+						Last Name
+					</label>
+					<div
+						className={`line h-[2px] w-full bg-lime-400 transition-all -z-0 duration-200 ease-out absolute -bottom-0 peer-focus:h-full rounded-sm peer-valid:h-full ${
+							formState.errors.lastName
+								? 'bg-red-400'
+								: 'bg-lime-400'
+						}`}
+					></div>
+				</div>
+
+				<div className='feild-container relative p-2 w-1/5'>
+					<input
+						type='number'
+						id='age'
+						required
+						className=' bg-transparent outline-none px-1 peer relative text-slate-800 z-10 font-mono font-semibold w-full
+            '
+						{...register('age', { min: 18, max: 99 })}
+					></input>
+					<label
+						// for='ease-out'
+						className='absolute left-4 top-2 text-lime-400 peer-focus:text-sm peer-focus:-top-6 peer-valid:text-sm peer-valid:-top-6 transition-all duration-150'
+					>
+						Age
+					</label>
+					<div
+						className={`line h-[2px] w-full bg-lime-400 transition-all -z-0 duration-200 ease-out absolute -bottom-0 peer-focus:h-full rounded-sm peer-valid:h-full ${
+							formState.errors.age ? 'bg-red-400' : 'bg-lime-400'
+						}`}
+					></div>
+				</div>
+
+				<div className='feild-container relative p-2 w-1/5'>
+					<input
+						type='text'
+						id='address'
+						required
+						className=' bg-transparent outline-none px-1 peer relative text-slate-800 z-10 font-mono font-semibold w-full
+            '
+						{...register('ethAddress', {
+							required: true,
+							pattern: /^(0x)?[0-9a-fA-Z]{40}$/,
+						})}
+					></input>
+					<label
+						// for='ease-out'
+						className='absolute left-4 top-2 text-lime-400 peer-focus:text-sm peer-focus:-top-6 peer-valid:text-sm peer-valid:-top-6 transition-all duration-150'
+					>
+						Eth Address
+					</label>
+					<div
+						className={`line h-[2px] w-full bg-lime-400 transition-all -z-0 duration-200 ease-out absolute -bottom-0 peer-focus:h-full rounded-sm peer-valid:h-full ${
+							formState.errors.ethAddress
+								? 'bg-red-400'
+								: 'bg-lime-400'
+						}`}
+					></div>
+				</div>
+
+				<input
+					className='btn text-lime-400 hover:bg-lime-400 hover:text-black transition-all duration-150 active:scale-95 active:translate-y-1'
+					type='submit'
+				/>
+			</form>
+		</div>
+	)
 }
 
 export default Form
--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,127 +1,148 @@
-import detectEthereumProvider from "@metamask/detect-provider"
-import { Strategy, ZkIdentity } from "@zk-kit/identity"
-import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers, Contract, utils } from "ethers"
-import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
-import Head from "next/head"
-import React, { useEffect } from "react"
-import { ExternalProvider, Web3Provider } from "@ethersproject/providers";
-
-import styles from "../styles/Home.module.css"
+import detectEthereumProvider from '@metamask/detect-provider'
+import { Strategy, ZkIdentity } from '@zk-kit/identity'
+import { generateMerkleProof, Semaphore } from '@zk-kit/protocols'
+import { providers, Contract, utils } from 'ethers'
+import Greeter from 'artifacts/contracts/Greeters.sol/Greeters.json'
+import Head from 'next/head'
+import React, { useEffect } from 'react'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+	const [logs, setLogs] = React.useState('Connect your wallet and greet!')
 
-    const [greeterEvents, setGreeterEvents] = React.useState("Listening for new greetings...")
+	const [greeterEvents, setGreeterEvents] = React.useState<string[]>([])
 
-    const filter = {
-    address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
-    topics: [utils.id("NewGreeting(bytes32)")],
-  };
+	const [contract, setContract] = React.useState<Contract>()
+	let iter = 1
 
-    const checkEvents = async() => {
-      const provider = new providers.JsonRpcProvider("http://localhost:8545")
-      const greeterContract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi, provider)
+	useEffect(() => {
+		const provider = new providers.JsonRpcProvider('http://localhost:8545')
+		setContract(
+			new Contract(
+				'0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+				Greeter.abi,
+				provider
+			) as any
+		)
+	}, [])
 
-      console.log("CHECK EVENTS TRIGGERED")
-      greeterContract.on(filter, (greeting: string) => {
-          console.log(utils.parseBytes32String(greeting))
-          setGreeterEvents(utils.parseBytes32String(greeting))
-        })
-    // const provider = (await detectEthereumProvider()) as any;
-    //     await provider.request({ method: "eth_requestAccounts" });
-    //     const ethersProvider = new providers.Web3Provider(
-    //       provider as ExternalProvider
-    //     );
-    //     ethersProvider.on(filter, (log, event) => {
-    //       console.log(`log: ${utils.toUtf8String(log.data)}`);
-    //       setGreeterEvents(utils.toUtf8String(log.data));
-    //     });
-    }
+	useEffect(() => {
+		console.log(greeterEvents, 'event updated')
+	}, [greeterEvents.length])
 
-    useEffect(() => {
-      async function checkEventsCaller(){
-        //   setInterval(async ()=> await checkEvents(), 1000)
-      }
-    
-    //   checkEventsCaller()
-    }, [])
-    
+	useEffect(() => {
+		console.log(contract)
+		if (contract !== undefined) {
+			console.log('listening events...')
+			contract.on(contract.filters.NewGreeting(), (data: string) => {
+				let tempData = greeterEvents
+				let decodedData = utils.parseBytes32String(data)
+				tempData.push(decodedData)
+				setGreeterEvents(tempData)
+				// console.log(data)
+			})
+		}
+		return () => {
+			contract?.removeAllListeners('NewGreeting')
+		}
+	}, [contract])
 
+	async function greet() {
+		setLogs('Creating your Semaphore identity...')
 
-    async function greet() {
-        checkEvents()
-        setLogs("Creating your Semaphore identity...")
+		const provider = (await detectEthereumProvider()) as any
 
-        const provider = (await detectEthereumProvider()) as any
+		await provider.request({ method: 'eth_requestAccounts' })
 
-        await provider.request({ method: "eth_requestAccounts" })
+		const ethersProvider = new providers.Web3Provider(provider)
+		const signer = ethersProvider.getSigner()
+		const message = await signer.signMessage(
+			'Sign this message to create your identity!'
+		)
 
-        const ethersProvider = new providers.Web3Provider(provider)
-        const signer = ethersProvider.getSigner()
-        const message = await signer.signMessage("Sign this message to create your identity!")
+		const identity = new ZkIdentity(Strategy.MESSAGE, message)
+		const identityCommitment = identity.genIdentityCommitment()
+		const identityCommitments = await (
+			await fetch('./identityCommitments.json')
+		).json()
 
-        const identity = new ZkIdentity(Strategy.MESSAGE, message)
-        const identityCommitment = identity.genIdentityCommitment()
-        const identityCommitments = await (await fetch("./identityCommitments.json")).json()
+		const merkleProof = generateMerkleProof(
+			20,
+			BigInt(0),
+			identityCommitments,
+			identityCommitment
+		)
 
-        const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
+		setLogs('Creating your Semaphore proof...')
 
-        setLogs("Creating your Semaphore proof...")
+		const greeting = 'Hello world ' + String(iter)
 
-        const greeting = "Hello world"
+		const witness = Semaphore.genWitness(
+			identity.getTrapdoor(),
+			identity.getNullifier(),
+			merkleProof,
+			merkleProof.root,
+			greeting
+		)
 
-        const witness = Semaphore.genWitness(
-            identity.getTrapdoor(),
-            identity.getNullifier(),
-            merkleProof,
-            merkleProof.root,
-            greeting
-        )
+		const { proof, publicSignals } = await Semaphore.genProof(
+			witness,
+			'./semaphore.wasm',
+			'./semaphore_final.zkey'
+		)
+		const solidityProof = Semaphore.packToSolidityProof(proof)
 
-        const { proof, publicSignals } = await Semaphore.genProof(witness, "./semaphore.wasm", "./semaphore_final.zkey")
-        const solidityProof = Semaphore.packToSolidityProof(proof)
+		const response = await fetch('/api/greet', {
+			method: 'POST',
+			body: JSON.stringify({
+				greeting,
+				nullifierHash: publicSignals.nullifierHash,
+				solidityProof: solidityProof,
+			}),
+		})
 
-        const response = await fetch("/api/greet", {
-            method: "POST",
-            body: JSON.stringify({
-                greeting,
-                nullifierHash: publicSignals.nullifierHash,
-                solidityProof: solidityProof
-            })
-        })
+		if (response.status === 500) {
+			const errorMessage = await response.text()
 
-        if (response.status === 500) {
-            const errorMessage = await response.text()
+			setLogs(errorMessage)
+		} else {
+			setLogs('Your anonymous greeting is onchain :)')
+		}
+		iter++
+	}
 
-            setLogs(errorMessage)
-        } else {
-            setLogs("Your anonymous greeting is onchain :)")
-        }
-    }
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Greetings</title>
+				<meta
+					name='description'
+					content='A simple Next.js/Hardhat privacy application with Semaphore.'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Greetings</title>
-                <meta name="description" content="A simple Next.js/Hardhat privacy application with Semaphore." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+			<main className={styles.main}>
+				<h1 className={styles.title}>Greetings</h1>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Greetings</h1>
+				<p className={styles.description}>
+					A simple Next.js/Hardhat privacy application with Semaphore.
+				</p>
 
-                <p className={styles.description}>A simple Next.js/Hardhat privacy application with Semaphore.</p>
+				<div className={styles.logs}>{logs}</div>
 
-                <div className={styles.logs}>{logs}</div>
-
-                <div onClick={() => greet()} className={styles.button}>
-                    Greet
-                </div>
-                
-            </main>
-            <button onClick={checkEvents} className='border-2 p-2'>click</button>
-            <textarea id="message" readOnly value={greeterEvents} className="block p-2.5 w-full text-sm rounded-sm bg-black text-white" placeholder="Listening for new greetings..."></textarea>
-        </div>
-    )
+				<div onClick={() => greet()} className={styles.button}>
+					Greet
+				</div>
+				<textarea
+					id='message'
+					value={greeterEvents}
+					className='block p-2.5 w-full text-sm rounded-sm bg-lime-400 text-slate-800 placeholder:text-slate-800 font-bold mt-16'
+					placeholder='Listening for new greetings...'
+					readOnly
+				></textarea>
+			</main>
+		</div>
+	)
 }
+// yarn dev then yarn deploy --network localhost
